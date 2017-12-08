a98bea78148c83dbc376e5fc2748cb831634c4e7
Lisa Lee <snickers495@gmail.com>
2017-12-08 23:23:48 +0000

    sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
+  before_action :require_login
+  def new
+  end
 
+  def create
+    session[:name] = params[:name]
+    redirect_to "/show"
+  end
 
-end 
+  def destroy
+    session.delete :name
+    redirect_to "/"
+  end
+
+end
